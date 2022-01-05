0f8efdf843021509bd7991d2976552105146ce77
BritoValter <valterblopes@gmail.com>
2022-01-05 01:04:04 +0000

    finish index.js

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ const   express = require('express'), //Allows to respond to HTTP requests, defi
 const   router = express(), //Instantiating Express
         server = http.createServer(router); //Instantiating the server
 router.use(express.static(path.resolve(__dirname,'views'))); //Serving static content from "views" folder
-router.use(express.json());
+router.get('/', function(req, res) {
+
+        res.writeHead(200, {'Content-Type' : 'text/html'}); //Tell the user that the resource exists and which type that is
+    
+        let xml = fs.readFileSync('classInVideo.xml', 'utf8'), //read in the XML file
+            xsl = fs.readFileSync('classInVideo.xsl', 'utf8'); //read in the XSL file
+    
+        console.log(xml);
+        console.log(xsl);
+    
+        let doc = xmlParse(xml), //Parse the XML file
+            stylesheet = xmlParse(xsl); //Parse the XSL file
+    
+        let result = xsltProcess(doc, stylesheet); //Performing XSLT
+    
+        console.log(result);
+    
+        res.end(result.toString()); //Serve back the user
+    
+    });
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+        const addr = server.address();
+        console.log('Server listening at', addr.address + ':' + addr.port)
